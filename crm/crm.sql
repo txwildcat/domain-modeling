2035c2231dfc3aa6b19069a3e20e1c47549b025f
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-01-20 03:19:20 +0000

    In class work 1.19.22

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,28 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  self_contact BIT
+);
+
+CREATE TABLE activity (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  contact_id INTEGER
+  activity_type TEXT,
+  date TEXT,
+  time TEXT,
+  notes TEXT
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  industry TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,28 @@
 
+CREATE TABLE profile (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT,
+first_name TEXT,
+last_name TEXT,
+email TEXT,
+location TEXT
+);
+
+CREATE TABLE user (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT,
+first_name TEXT,
+last_name TEXT,
+email TEXT,
+location TEXT
+);
+
+CREATE TABLE post (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT,
+first_name TEXT,
+last_name TEXT,
+email TEXT,
+location TEXT
+);
+
